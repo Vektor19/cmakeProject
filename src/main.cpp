9305d4ad09b2dcb62e970511cc4376764f4660fa
Vektor19 <viktor.komissarov@knu.ua>
2025-01-11 22:59:07 +0200

    added glad init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -18,12 +19,19 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+	
+	if (!gladLoadGL())
+	{
+		std::cout<<"Can't load GLAD"<< std::endl;
+		return -1;
+	}
+	std::cout<<"OpenGL "<<GLVersion.major<<"."<<GLVersion.minor<<std::endl;
     /* Loop until the user closes the window */
+	glClearColor(0,1,0,1);
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
