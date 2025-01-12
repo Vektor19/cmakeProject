8ffc96fadfec88c522da9b9cc0f7d95b8a86a0a7
Vektor19 <viktor.komissarov@knu.ua>
2025-01-12 15:16:32 +0200

    added sample triangle drawing

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "vetal loh", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -27,12 +27,89 @@ int main(void)
 		return -1;
 	}
 	std::cout<<"OpenGL "<<GLVersion.major<<"."<<GLVersion.minor<<std::endl;
+    glfwSwapInterval(1);
+
+
+
+    GLfloat points[] = {
+        0.0f, 0.5f, 0.0f,
+       -0.5f, 0.0f, 0.0f,
+        0.5f, 0.0f, 0.0f,
+    };
+    GLfloat colors[] = {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+    };
+
+    const char* v_shader_source =
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertex_position;"
+        "layout(location = 1) in vec3 vertex_color;"
+        "out vec3 color;"
+        "void main(){"
+        "   gl_Position = vec4(vertex_position, 1.0);"
+        "   color = vertex_color;"
+        "}";
+
+    const char* f_shader_source =
+        "#version 460\n"
+        "in vec3 color;"
+        "out vec4 fragment_color;"
+        "void main(){"
+        "   fragment_color = vec4(color, 1.0);"
+        "}";
+
+    GLuint v_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(v_shader, 1, &v_shader_source, nullptr);
+    glCompileShader(v_shader);
+
+    GLuint f_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(f_shader, 1, &f_shader_source, nullptr);
+    glCompileShader(f_shader);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, v_shader);
+    glAttachShader(shader_program, f_shader);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(v_shader);
+    glDeleteShader(f_shader);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+
+    float angle = 0;
     /* Loop until the user closes the window */
-	glClearColor(0,1,0,1);
+	glClearColor(1,1,0,1);
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
