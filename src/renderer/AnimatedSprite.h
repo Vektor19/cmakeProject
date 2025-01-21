e4e264e9808efbf22b88ce7429113be755a5a7ef
Vektor19 <viktor.komissarov@knu.ua>
2025-01-21 22:01:12 +0200

    added AnimatedSprite class to header file and basic impl

--- src/renderer/AnimatedSprite.h
+++ src/renderer/AnimatedSprite.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Sprite.h"
+namespace renderer
+{
+	class Sprite;
+
+	class AnimatedSprite: public Sprite
+	{
+	public:
+		AnimatedSprite();
+		~AnimatedSprite();
+	private:
+
+	};
+}
