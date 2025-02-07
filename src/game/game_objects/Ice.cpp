c1b7fe1c3eeefeeb272aa4a7d46c784830e2550a
Vektor19 <viktor.komissarov@knu.ua>
2025-02-07 12:24:09 +0200

    added Ice object

---
+++ src/game/game_objects/Ice.cpp
@@ -0,0 +1,33 @@
+#include "Ice.h"
+#include "../../renderer/Sprite.h"
+#include "../../resources/ResourceManager.h"
+
+Ice::Ice(const glm::vec2& position,
+	const glm::vec2& size,
+	const float rotation)
+	: GameObject(position, size, rotation)
+	, m_blockOffsets { glm::vec2(0, m_size.y / 2.f),
+					   glm::vec2(m_size.x / 2.f, m_size.y / 2.f),
+					   glm::vec2(0, 0),
+					   glm::vec2(m_size.x / 2.f, 0) }
+	, m_sprite(resources::ResourcesManager::getSprite("ice"))
+{
+	
+}
+
+void Ice::render() const
+{
+	renderBlock(EBlockLocation::TopLeft);
+	renderBlock(EBlockLocation::TopRight);
+	renderBlock(EBlockLocation::BottomLeft);
+	renderBlock(EBlockLocation::BottomRight);
+}
+
+void Ice::update(const uint64_t delta)
+{
+}
+
+void Ice::renderBlock(EBlockLocation eBlockLocation) const
+{
+	m_sprite->render(m_position + m_blockOffsets[static_cast<size_t>(eBlockLocation)], m_size / 2.f, m_rotation);
+}
---
+++ src/game/game_objects/Ice.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "GameObject.h"
+#include <memory>
+#include <array>
+namespace renderer
+{
+	class Sprite;
+}
+class Ice : public GameObject
+{
+public:
+	enum class EBlockLocation {
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight
+	};
+	Ice(const glm::vec2& position,
+		const glm::vec2& size,
+		const float rotation = 0);
+	void render() const override;
+	void update(const uint64_t delta) override;
+
+private:
+	void renderBlock(EBlockLocation eBlockLocation) const;
+	std::shared_ptr<renderer::Sprite> m_sprite;
+	std::array<glm::vec2, 4> m_blockOffsets;
+};
