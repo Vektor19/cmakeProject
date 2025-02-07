5b63464443c716490e4a5a8e4dd65139f9993121
Vektor19 <viktor.komissarov@knu.ua>
2025-02-07 12:23:32 +0200

    added BetonWall class

---
+++ src/game/game_objects/BetonWall.cpp
@@ -0,0 +1,77 @@
+#include "BetonWall.h"
+#include "../../renderer/Sprite.h"
+#include "../../resources/ResourceManager.h"
+
+BetonWall::BetonWall(const EBetonWallType eBetonWallType,
+	const glm::vec2& position,
+	const glm::vec2& size,
+	const float rotation)
+	: GameObject(position, size, rotation)
+	, m_eBrickStates{EBrickState::Destroyed,
+					 EBrickState::Destroyed,
+					 EBrickState::Destroyed,
+					 EBrickState::Destroyed }
+	, m_blockOffsets { glm::vec2(0, m_size.y / 2.f),
+					   glm::vec2(m_size.x / 2.f, m_size.y / 2.f),
+					   glm::vec2(0, 0),
+					   glm::vec2(m_size.x / 2.f, 0) }
+{
+	m_sprites[static_cast<size_t>(EBrickState::All)] = resources::ResourcesManager::getSprite("betonWall");
+	switch (eBetonWallType)
+	{
+	case EBetonWallType::All:
+		m_eBrickStates.fill(EBrickState::All);
+		break;
+	case EBetonWallType::Top:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::TopLeft)]	 = EBrickState::All;
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::TopRight)]	 = EBrickState::All;
+		break;
+	case EBetonWallType::Bottom:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::BottomLeft)]  = EBrickState::All;
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::BottomRight)] = EBrickState::All;
+		break;
+	case EBetonWallType::Left:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::TopLeft)]	 = EBrickState::All;
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::BottomLeft)]  = EBrickState::All;
+		break;
+	case EBetonWallType::Right:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::TopRight)]	 = EBrickState::All;
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::BottomRight)] = EBrickState::All;
+		break;
+	case EBetonWallType::TopLeft:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::TopLeft)]	 = EBrickState::All;
+		break;
+	case EBetonWallType::TopRight:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::TopRight)]	 = EBrickState::All;
+		break;
+	case EBetonWallType::BottomLeft:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::BottomLeft)]	 = EBrickState::All;
+		break;
+	case EBetonWallType::BottomRight:
+		m_eBrickStates[static_cast<size_t>(EBrickLocation::BottomRight)] = EBrickState::All;
+		break;
+	default:
+		break;
+	}
+}
+
+void BetonWall::render() const
+{
+	renderBrick(EBrickLocation::TopLeft);
+	renderBrick(EBrickLocation::TopRight);
+	renderBrick(EBrickLocation::BottomLeft);
+	renderBrick(EBrickLocation::BottomRight);
+}
+
+void BetonWall::update(const uint64_t delta)
+{
+}
+
+void BetonWall::renderBrick(EBrickLocation eBrickLocation) const
+{
+	const EBrickState state = m_eBrickStates[static_cast<size_t>(eBrickLocation)];
+	if (state != EBrickState::Destroyed)
+	{
+		m_sprites[static_cast<size_t>(state)]->render(m_position + m_blockOffsets[static_cast<size_t>(eBrickLocation)], m_size/2.f, m_rotation);
+	}
+}
---
+++ src/game/game_objects/BetonWall.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "GameObject.h"
+#include <memory>
+#include <array>
+namespace renderer
+{
+	class Sprite;
+}
+class BetonWall : public GameObject
+{
+public:
+	enum class EBetonWallType {
+		All,
+		Top,
+		Bottom,
+		Left,
+		Right,
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight,
+	};
+	enum class EBrickState {
+		All,
+		Destroyed
+	};
+	enum class EBrickLocation {
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight
+	};
+	BetonWall(const EBetonWallType eBetonWallType,
+		const glm::vec2& position,
+		const glm::vec2& size,
+		const float rotation = 0);
+	void render() const override;
+	void update(const uint64_t delta) override;
+
+private:
+	void renderBrick(EBrickLocation eBrickLocation) const;
+	std::array<std::shared_ptr<renderer::Sprite>, 1> m_sprites;
+	std::array<EBrickState, 4> m_eBrickStates;
+	std::array<glm::vec2, 4> m_blockOffsets;
+};
