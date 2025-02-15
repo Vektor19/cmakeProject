688ff2ac184adaefe7baebb2742f0ea849ec60b8
Vektor19 <viktor.komissarov@knu.ua>
2025-02-15 15:41:08 +0200

    added collision handling and destroing Trees object

--- src/game/game_objects/Trees.cpp
+++ src/game/game_objects/Trees.cpp
@@ -1,8 +1,8 @@
 #include "Trees.h"
 #include "../../renderer/Sprite.h"
 #include "../../resources/ResourceManager.h"
-
 #include "../../physics/collision/AABBCollider.h"
+#include "Bullet.h"
 Trees::Trees(const glm::vec2& position,
 	const glm::vec2& size,
 	const float rotation,
@@ -12,6 +12,7 @@ Trees::Trees(const glm::vec2& position,
 					   glm::vec2(m_size.x / 2.f, m_size.y / 2.f),
 					   glm::vec2(0, 0),
 					   glm::vec2(m_size.x / 2.f, 0) }
+	, m_eTreesState(ETreesState::All)
 	, m_sprite(resources::ResourcesManager::getSprite("trees"))
 {
 	int collisionMask =
@@ -23,14 +24,31 @@ Trees::Trees(const glm::vec2& position,
 
 void Trees::render() const
 {
-	renderBlock(EBlockLocation::TopLeft);
-	renderBlock(EBlockLocation::TopRight);
-	renderBlock(EBlockLocation::BottomLeft);
-	renderBlock(EBlockLocation::BottomRight);
+	if (m_eTreesState != ETreesState::Destroyed)
+	{
+		renderBlock(EBlockLocation::TopLeft);
+		renderBlock(EBlockLocation::TopRight);
+		renderBlock(EBlockLocation::BottomLeft);
+		renderBlock(EBlockLocation::BottomRight);
+	}
 }
 
 void Trees::update(const double delta)
 {
+}
+
+void Trees::OnCollisionCallback(const std::shared_ptr<GameObject> object, const glm::vec2& collisionPoint)
+{
+	if (auto bullet = std::dynamic_pointer_cast<Bullet>(object))
+	{
+		takeDamage();
+	}
+}
+
+void Trees::takeDamage()
+{
+	m_eTreesState = ETreesState::Destroyed;
+	m_colliders.front()->setCollisionLayer(physics::Collider::CollisionLayer::None);
 }
 
 void Trees::renderBlock(EBlockLocation eBlockLocation) const
--- src/game/game_objects/Trees.h
+++ src/game/game_objects/Trees.h
@@ -15,15 +15,22 @@ public:
 		BottomLeft,
 		BottomRight
 	};
+	enum class ETreesState {
+		All,
+		Destroyed
+	};
 	Trees(const glm::vec2& position,
 		const glm::vec2& size,
 		const float rotation = 0,
 		const float layer = 0);
 	void render() const override;
 	void update(const double delta) override;
+	virtual void OnCollisionCallback(const std::shared_ptr<GameObject> object, const glm::vec2& collisionPoint) override;
+	virtual void takeDamage();
 
 private:
 	void renderBlock(EBlockLocation eBlockLocation) const;
 	std::shared_ptr<renderer::Sprite> m_sprite;
 	std::array<glm::vec2, 4> m_blockOffsets;
+	ETreesState m_eTreesState;
 };
