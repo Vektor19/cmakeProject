230560d8b9fd6e0d52aa59041366cfe50efc3005
Vektor19 <viktor.komissarov@knu.ua>
2025-06-22 19:38:53 +0300

    feat: add durability to eagle

--- src/game/game_objects/Eagle.cpp
+++ src/game/game_objects/Eagle.cpp
@@ -1,14 +1,18 @@
 #include "Eagle.h"
 #include "../../renderer/Sprite.h"
 #include "../../resources/ResourceManager.h"
-
 #include "../../physics/collision/AABBCollider.h"
+#include "../../physics/PhysicsEngine.h"
+#include "../DynamicObjectsRenderer.h"
+#include "Bullet.h"
 Eagle::Eagle(const glm::vec2& position,
 	const glm::vec2& size,
 	const float rotation,
-	const float layer)
+	const float layer,
+	const double durability)
 	: GameObject(position, size, rotation, layer)
 	, m_eCurrentEagleState(EEagleState::Alive)
+	, m_durability(durability)
 {
 	m_sprites[static_cast<size_t>(EEagleState::Alive)] = resources::ResourcesManager::getSprite("eagle");
 	m_sprites[static_cast<size_t>(EEagleState::Dead)]  = resources::ResourcesManager::getSprite("eagle_dead");
@@ -24,6 +28,24 @@ Eagle::Eagle(const glm::vec2& position,
 void Eagle::render() const
 {
 	m_sprites[static_cast<size_t>(m_eCurrentEagleState)]->render(m_position, m_size, m_rotation, m_layer);
+}
+
+void Eagle::OnCollisionCallback(const std::shared_ptr<GameObject> object, const glm::vec2& collisionPoint)
+{
+	if (auto bullet = std::dynamic_pointer_cast<Bullet>(object))
+	{
+		takeDamage(bullet->getDamage());
+	}
+}
+
+void Eagle::takeDamage(const double damage)
+{
+	m_durability -= damage;
+	if (m_durability <= 0)
+	{
+		m_isAlive = false;
+		m_eCurrentEagleState = EEagleState::Dead;
+	}
 }
 
 void Eagle::update(const double delta)
--- src/game/game_objects/Eagle.h
+++ src/game/game_objects/Eagle.h
@@ -16,11 +16,15 @@ public:
 	Eagle(const glm::vec2& position,
 		const glm::vec2& size,
 		const float rotation = 0,
-		const float layer = 0);
+		const float layer = 0,
+		const double durability = 100);
 	void render() const override;
+	virtual void OnCollisionCallback(const std::shared_ptr<GameObject> object, const glm::vec2& collisionPoint) override;
+	virtual void takeDamage(const double damage);
 	void update(const double delta) override;
 
 private:
 	std::array<std::shared_ptr<renderer::Sprite>, 2> m_sprites;
 	EEagleState m_eCurrentEagleState;
+	double m_durability;
 };
